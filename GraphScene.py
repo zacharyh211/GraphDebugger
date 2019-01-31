45e6957ec27e37eb937be79588f9b28cac5d39b5
ZacharyHancock <zacharyh211@gmail.com>
2019-01-31 03:19:28 -0600

    Add initialization to GraphScene and event prints

--- GraphScene.py
+++ GraphScene.py
@@ -8,15 +8,24 @@ from PyQt5.QtCore import Qt, QLineF
 
 
 class GraphicNode(QGraphicsEllipseItem):
+	r = 40
 
-	def __init__(self,x,y,w,h):
-		super().__init__(x,y,w,h)
+	def __init__(self,point):
+		super().__init__(point.x()-r,point.y()-r,2*r,2*r)
 		self.setBrush(QBrush(Qt.light_blue))
 		self.empty = True
 		self.edge_graphics = []
 
 	def center(self):
 		return self.sceneBoundingRect().center()
+
+class GraphicEdge(QGraphicLineItem):
+
+	def __init__(self,line,src):
+		super().__init__(line)
+		self.setZValue(-100)
+		self.source_node = src
+		self.target_node = None
 
 class GraphScene(QGraphicsScene):
 
@@ -37,10 +46,16 @@ class GraphScene(QGraphicsScene):
 		if self.editing:
 			pass
 
+
 	def mousePressEvent(self, event):
 		print('clicked')
 		if self.editing:
 			pass
+
+			item = self.itemAt(event.scenePos(), QTransform())
+
+			if not item:
+				self.addItem(GraphicNode(event.scenePos()))
 
 	def mouseMoveEvent(self, event):
 		print('moving')
